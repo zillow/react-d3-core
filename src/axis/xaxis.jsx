6ed0552792e8ff3876fe5d0d4fca53ca045c70b3
Rob McGuire-Dale <robmc@zillow.com>
2016-05-09 16:02:57 -0700

    Merge custom xaxis changes with border

--- src/axis/xaxis.jsx
+++ src/axis/xaxis.jsx
@@ -49,13 +49,16 @@ export default class Xaxis extends Component {
     xTickPadding: PropTypes.number,
     xTickFormat: PropTypes.func,
     xTicks: PropTypes.array,
+    xLabel: PropTypes.string,
+    xLabelPosition: PropTypes.oneOf(['top', 'bottom', 'left', 'right', 'inline-left']),
+    labelOffset: PropTypes.number,
     xTickExclude: PropTypes.array,
     style: PropTypes.object,
     xTickValues: PropTypes.array
   }
 
   render() {
-    var {
+    const {
       height,
       width,
       margins,
@@ -63,7 +66,6 @@ export default class Xaxis extends Component {
       x,
       xAxisClassName,
       xDomain,
-      xRange,
       xBandPaddingInner,
       xBandPaddingOuter,
       xScale,
@@ -82,8 +84,10 @@ export default class Xaxis extends Component {
       xTickValues
     } = this.props;
 
-    var t;
-    var axisLabel;
+    let { xRange } = this.props;
+
+    let t = null;
+    let axisLabel = null;
 
     if(!xRange) {
       xRange = [0, width - margins.left - margins.right];
@@ -99,28 +103,41 @@ export default class Xaxis extends Component {
 
     if(xLabel) {
       axisLabel = <Label
-        height= {height}
-        width= {width}
-        margins= {margins}
-        labelTitle= {xLabel}
-        labelPosition= {xLabelPosition}
-        labelOffset= {labelOffset}
-        bandPaddingInner= {xBandPaddingInner}
-        bandPaddingOuter= {xBandPaddingOuter}
+        height = {height}
+        width = {width}
+        margins = {margins}
+        labelTitle = {xLabel}
+        labelPosition = {xLabelPosition}
+        labelOffset = {labelOffset}
+        bandPaddingInner = {xBandPaddingInner}
+        bandPaddingOuter = {xBandPaddingOuter}
         />
     }
 
+    const axisBorder = (
+      <line
+        className='react-d3-core__axis__xAxis__border'
+        x2={width - margins.right}
+        y2='0'
+        style={{
+          transform: `translateX(${-margins.left}px)`
+        }}
+      />
+    );
+
     return (
-      <g transform= {t}>
+      <g
+        className={xAxisClassName}
+        transform={t} >
+        {axisBorder}
         <Axis
-          height= {height}
-          width= {width}
-          margins= {margins}
-          showAxis= {showXAxis}
-          axisClassName= {xAxisClassName}
-          bandPaddingInner= {xBandPaddingInner}
-          bandPaddingOuter= {xBandPaddingOuter}
-          type = "x"
+          height = {height}
+          width = {width}
+          margins = {margins}
+          showAxis = {showXAxis}
+          bandPaddingInner = {xBandPaddingInner}
+          bandPaddingOuter = {xBandPaddingOuter}
+          type = 'x'
           proxy = {x}
           domain = {xDomain}
           range = {xRange}
@@ -134,8 +151,7 @@ export default class Xaxis extends Component {
           ticks = {xTicks}
           style = {style}
           tickExclude = {xTickExclude}
-          tickValues = {xTickValues}
-          />
+        />
         {axisLabel}
       </g>
     )
